2053a85cb648c63cc60b57058b6488b56eaa60a1
Ramon Santos <135469708+RamonSantosdaSilva@users.noreply.github.com>
2024-09-15 01:35:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Função para somar dois números
+function soma(a, b) {
+    return a + b;
+}
+
+// Função para subtrair dois números
+function subtracao(a, b) {
+    return a - b;
+}
+
+// Testando as funções
+let numero1 = 10;
+let numero2 = 5;
+
+console.log("Soma: " + soma(numero1, numero2)); // Saída: Soma: 15
+console.log("Subtração: " + subtracao(numero1, numero2)); // Saída: Subtração: 5
